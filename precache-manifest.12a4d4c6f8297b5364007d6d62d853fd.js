3579dd5c3c7f277bfbf5c3efbed05f33f6c19818
Junmin Ahn <junminahn@outlook.com>
2020-07-07 08:08:38 +0000

    CircleCI Pipeline

---
+++ precache-manifest.12a4d4c6f8297b5364007d6d62d853fd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18c199e84dd4c9c8f5ad578c3e75339",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e27fa448eb74c92ef4e4",
+    "url": "/static/css/2.288be84d.chunk.css"
+  },
+  {
+    "revision": "8c776d03a0009e25fea6",
+    "url": "/static/css/main.daa58a73.chunk.css"
+  },
+  {
+    "revision": "e27fa448eb74c92ef4e4",
+    "url": "/static/js/2.602e0807.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.602e0807.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c776d03a0009e25fea6",
+    "url": "/static/js/main.b36f76bf.chunk.js"
+  },
+  {
+    "revision": "57b2bd23eab31322d78a",
+    "url": "/static/js/runtime-main.89f1cf4a.js"
+  },
+  {
+    "revision": "9d1dbc462dc0d2dcb75046bdaf493e8a",
+    "url": "/static/media/budwiz.9d1dbc46.jpg"
+  },
+  {
+    "revision": "b1c25a36e88f5fa8a585647fa1c2b286",
+    "url": "/static/media/budwiz.b1c25a36.png"
+  },
+  {
+    "revision": "5a5127efbbcc55aa16f92f37ae1a4775",
+    "url": "/static/media/computer.5a5127ef.png"
+  },
+  {
+    "revision": "9828c70d93ee2438276a2e7f18aad8e3",
+    "url": "/static/media/computer.9828c70d.jpg"
+  },
+  {
+    "revision": "938ef10ff845bcc342de86c304a5437c",
+    "url": "/static/media/hangman.938ef10f.png"
+  },
+  {
+    "revision": "d0cd42c2a08fdb14f9ced4053ea8cbec",
+    "url": "/static/media/hangman.d0cd42c2.jpg"
+  }
+]);
